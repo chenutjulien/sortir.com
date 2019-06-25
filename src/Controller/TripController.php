ed5c9b9b4d649cc8ffed7dda935aba730e67e042
Laure <laure.coussout@free.fr>
2019-06-25 17:03:50 +0200

    Formulaire Trip en  route
    mais besoin de debug

--- src/Controller/TripController.php
+++ src/Controller/TripController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Trip;
+use App\Form\TripType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class TripController extends Controller
@@ -14,4 +18,25 @@ class TripController extends Controller
     {
         return $this->render('trip/trip.html.twig');
     }
+
+
+/**
+ * @Route("/proposerSortie", name="trip_create")
+ */
+    public function createTrip(EntityManagerInterface $em, Request $rq){
+        $trip= new Trip();
+        $tripForm= $this->createForm(TripType::class,$trip);
+        $tripForm=handleRequest($rq);
+
+        if($tripForm->isSubmitted()&& $tripForm->isValid()){
+            $em->persist($trip);
+            $em->flush();
+            $this->addFlash("success","Votre sortie est bien enregistrée");
+            return $this-> redirectToRoute("trip_details",['id'=> $trip->getId()]);
+            //Besoin de creer ce chemin (vers le détails de la sortie)+ pas sur que cela fonctionne!!
+        }
+        return $this->render("trip/create.html.twig", [
+            "tripForm"=>$tripForm->createView()
+        ]);
+}
 }
--- src/Form/TripType.php
+++ src/Form/TripType.php
@@ -3,7 +3,12 @@
 namespace App\Form;
 
 use App\Entity\Trip;
+
+
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,13 +17,29 @@ class TripType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('startDateTime')
-            ->add('duration')
-            ->add('maxRegistration')
-            ->add('informations')
-            ->add('cancelReason')
-            ->add('organiser')
+            ->add('name',TextType::class, [
+                'label'=>'Nom de la sortie',
+                'attr'=>['placeholder'=>'Insérez le titre de votre évènement ici...'],
+                'required'=>true])
+            ->add('startDateTime',DateType::class, [
+                'label'=>'Heure et Date de départ',
+                'required'=>'true'
+            ])
+            ->add('duration', IntegerType::class, [
+                'label'=>'Durée',
+                'attr'=>['placeholder'=>'En minutes'],
+                'required'=>true,
+            ])
+            ->add('maxRegistration', IntegerType::class, [
+                'label'=>'Nombre de personnes maximum',
+                'required'=>'true'
+            ])
+            ->add('informations', TextType::class, [
+                'label'=>'Description',
+                'attr'=>['placeholder'=>"Détaillez l'activité proposée"]
+            ])
+            ->add('cancelReason')// Sera demandée si l'organisateur annule la sortie
+            ->add('organiser')//Nous n'utiliserons pas les variables à partir de là (=clefs)
             ->add('registereds')
             ->add('state')
             ->add('spot')
