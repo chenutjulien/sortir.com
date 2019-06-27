696ae7a3cd89ede1046fed93550dc0b2d6686bad
chenutjulien <julien.chenut2019@campus-eni.fr>
2019-06-27 10:21:33 +0200

    <wxw<x

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -46,24 +46,4 @@ class ProfilController extends Controller
 //            'editForm' => $editForm->createView()
 //        ));
 //    }
-    /**
-     * @Route("/register", name="profil_profil")
-     */
-    public function register(EntityManagerInterface $em, Request $request,UserPasswordEncoderInterface $encoder)
-    {
-        $user = new User();
-        $registerForm = $this->createForm(RegisterType::class, $user);
-        $registerForm->handleRequest($request);
-        if ($registerForm->isSubmitted() && $registerForm->isValid()){
-            $hash=$encoder->encodePassword($user, $user->getPassword());
-            $user->setPassword($hash);
-            $em->persist($user);
-            $em->flush();
-            $this->addFlash('success', 'Inscription  validée');
-            $this->redirectToRoute("user_register");
-        }
-        return $this->render('user/register.html.twig', [
-            'formRegister'=>$registerForm->createView()
-        ]);
-    }
 }
