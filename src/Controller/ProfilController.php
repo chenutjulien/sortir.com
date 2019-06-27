38c6f942982effdaecedbd440f6f16a99e5c7254
chenutjulien <julien.chenut2019@campus-eni.fr>
2019-06-27 10:59:40 +0200

    vcvc

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -14,36 +14,36 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class ProfilController extends Controller
 {
-    /**
-     * @Route("/validProfil", name="profil_profil")
-     */
-    public function validProfil()
-    {
-
-        return $this->render('profil/profil.html.twig');
-    }
+//    /**
+//     * @Route("/validProfil", name="profil_index")
+//     */
+//    public function validProfil()
+//    {
+//
+//        return $this->render('profil/index.html.twig');
+//    }
 
 /* Fonction qui permettra de modifier le profil de l(utilisateur)*/
-//    /**
-//     * @Route("/modifyProfil", name="profil_profil")
-//     * @ParamConverter("user", options={"mapping": {"user_register" : "user"}})
-//     * @ParamConverter("modify", options={"mapping": {"user_register" : "id"}})
-//     */
-//    public function modifyProfil(Request $request, User $id)
-//    {
-//        $editForm = $this->createForm('AppBundle\Form\UserType', $id);
-//        $editForm->handleRequest($request);
-//
-//        if ($editForm->isSubmitted() && $editForm->isValid()) {
-//            $em = $this->getDoctrine()->getManager();
-//            $em->persist($id);
-//            $em->flush();
-//
-//            return $this->redirectToRoute('profil_profil');
-//        }
-//        return $this->render('profil/profil.html.twig', array(
-//            'id' => $id,
-//            'editForm' => $editForm->createView()
-//        ));
-//    }
+    /**
+     * @Route("/modifyProfil", name="profil_profil")
+     * @ParamConverter("user", options={"mapping": {"user_register" : "user"}})
+     * @ParamConverter("modify", options={"mapping": {"user_register" : "id"}})
+     */
+    public function modifyProfil(Request $request, User $id)
+    {
+        $editForm = $this->createForm('AppBundle\Form\UserType', $id);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($id);
+            $em->flush();
+
+            return $this->redirectToRoute('profil_profil');
+        }
+        return $this->render('profil/profil.html.twig', array(
+            'id' => $id,
+            'editForm' => $editForm->createView()
+        ));
+    }
 }
