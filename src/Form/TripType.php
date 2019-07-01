d408c20a728573d9472036537f50fa1e4a3644f5
JBCDA07 <johann.brochier2019@campus-eni.fr>
2019-07-01 16:57:20 +0200

    Update TripType.php

--- src/Form/TripType.php
+++ src/Form/TripType.php
@@ -17,57 +17,52 @@ class TripType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name',TextType::class, [
-                'label'=>'Nom de la sortie',
-                'attr'=>['placeholder'=>'Insérez le titre de votre évènement ici...'],
-                'required'=>true
+            ->add('name', TextType::class, [
+                'label' => 'Nom de la sortie',
+                'attr' => ['placeholder' => 'Insérez le titre de votre évènement ici...'],
+                'required' => true
+            ])
+            ->add('startDateTime', DateTimeType::class, [
+                'label' => 'Heure et date de départ',
+                'widget' => 'single_text',
+                'format' => 'd-M-y HH:mm:ss',
+                'html5' => false,
             ])
 
 
 
-            ->add('startDateTime',DateTimeType::class, [
-                'label'=>'Heure et date de départ',
-                'widget' => 'single_text',
-                'html5' => false,
-                'attr' => ['class' => 'input-append'],
-                'required'=>'true',
 
 //                'attr'=>['class'=>'form-control datetimepicker-input']
                 //-> CI-DESSUS POUR LE DATE PICKER
 
-            ])
+
             ->add('endDateTime', DateTimeType::class, [
-                'label'=>'Heure et date de fin',
-                'widget'=>'choice',
-                'format'=> 'd-M-y HH:mm:ss',
-                'model_timezone'=> 'Europe/Paris',
-                'required'=>true,
-            ])
-            ->add('maxRegistration', IntegerType::class, [
-                'label'=>'Nombre de personnes maximum',
-                'required'=>'true'
-            ])
-            ->add('informations', TextareaType::class, [
-                'label'=>'Description',
-                'attr'=>['placeholder'=>"Détaillez l'activité proposée"]
-            ])
-
-            ->add('Spot', EntityType::class, [
-                'class' => 'App\Entity\Spot',
-                'choice_label' => 'name',
-                'placeholder' => 'Choisir un lieu',
-                'label' => 'Lieu :'
-            ])
-
-            ->add('State', EntityType::class, [
-                'class' => 'App\Entity\State',
-                'choice_label' => 'name',
-                'data' => 'Créée',
-                'label' => 'Statut :'
-            ])
-
-
-        ;
+        'label' => 'Heure et date de fin',
+        'widget' => 'choice',
+        'format' => 'd-M-y HH:mm:ss',
+        'model_timezone' => 'Europe/Paris',
+        'required' => true,
+    ])
+        ->add('maxRegistration', IntegerType::class, [
+            'label' => 'Nombre de personnes maximum',
+            'required' => 'true'
+        ])
+        ->add('informations', TextareaType::class, [
+            'label' => 'Description',
+            'attr' => ['placeholder' => "Détaillez l'activité proposée"]
+        ])
+        ->add('Spot', EntityType::class, [
+            'class' => 'App\Entity\Spot',
+            'choice_label' => 'name',
+            'placeholder' => 'Choisir un lieu',
+            'label' => 'Lieu :'
+        ])
+        ->add('State', EntityType::class, [
+            'class' => 'App\Entity\State',
+            'choice_label' => 'name',
+            'data' => 'Créée',
+            'label' => 'Statut :'
+        ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
